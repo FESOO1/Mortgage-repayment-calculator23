01cab4a8347c7c26698613418b333a9bedc4fa8b
FES001 <firdavs7794948@gmail.com>
2025-03-04 21:21:25 +0500

    Working on it.

---
+++ script.js
@@ -0,0 +1,10 @@
+const clearEverythingButton = document.querySelector('#clearEverythingButton');
+const calculateMortgageButton = document.querySelector('#calculateMortgageButton');
+
+// INPUTS
+const mortgageAmountInput = document.querySelector('#mortgageAmountInput');
+const mortgageTermInput = document.querySelector('#mortgageTermInput');
+const interestRateInput = document.querySelector('#interestRateInput');
+const repaymentInput = document.querySelector('#repaymentInput');
+const interestOnlyInput = document.querySelector('#interestOnlyInput');
+const radioInputs = document.querySelectorAll('.mortgage-calculator-left-form-input-radio-itself');
